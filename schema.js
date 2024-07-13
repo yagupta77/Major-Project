63a1516cda48d8363c85e69c432299c4d38e023a
ygupta <156329651+yagupta77@users.noreply.github.com>
2024-07-13 11:00:27 +0530

    commit

--- schema.js
+++ schema.js
@@ -14,7 +14,7 @@ module.exports.listingSchema = Joi.object({
  module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required(),
-        comment: Joi.string().required
+        comment: Joi.string().required()
 
     }).required(),
  })
