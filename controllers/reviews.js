6aedf56f8df0f7781ff3037603f269254e87767a
ygupta <156329651+yagupta77@users.noreply.github.com>
2024-07-26 21:05:36 +0530

    commit

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,29 +1,17 @@
-const Listing = require("../models/listing"); // Ensure the Listing model is imported
+const Listing = require("../models/listing"); 
 const Review = require("../models/review");
 
-module.exports.post = async (req, res) => {
-    try {
-        let listing = await Listing.findById(req.params.id);
-        if (!listing) {
-            req.flash("error", "Listing not found");
-            return res.redirect("/listings");
-        }
-        
-        let newReview = new Review(req.body.review);
-        newReview.author = req.user._id;
-        listing.reviews.push(newReview);
-
-        await newReview.save();
-        await listing.save();
-        
-        req.flash("success", "New Review Created");
-        res.redirect(`/listings/${listing.id}`);
-    } catch (err) {
-        console.error(err);
-        req.flash("error", "Something went wrong");
-        res.redirect("/listings");
-    }
-};
+module.exports.post = 
+  (async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save({ validateBeforeSave: false });
+    req.flash("success", "New Review Created");
+    res.redirect(`/listings/${listing._id}`);
+  })
 
 module.exports.delete = async (req, res) => {
     try {
